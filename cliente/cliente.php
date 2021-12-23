f0d4de7742e94c4df98d77a7968d3c6b03c61840
AxelCalvo <84056595+AxelCalvo@users.noreply.github.com>
2021-12-22 22:30:47 -0600

    style

--- cliente/cliente.php
+++ cliente/cliente.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['Id_Usuario']) && isset($_SESSION['nombre'])) {
         <title>Datos Cliente</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        
         <link href="../style_m.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         
